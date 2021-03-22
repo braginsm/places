fc3b9bc80cf46409c08d5e0ad14d2aa847337130
Serg <braginsm@gmail.com>
2021-03-22 09:10:32 +0400

    Text и TextStyle

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -11,6 +11,23 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).canvasColor,
+        shadowColor: Colors.transparent,
+        toolbarHeight: 458, //высота аппбара в пикелях из фигмы
+        title: Container(
+          padding: EdgeInsets.fromLTRB(16, 64, 16, 322),
+          child: Text("Список интересных мест",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              color: Color(0xff252849),
+              fontWeight: FontWeight.w700,
+              fontSize: 32,
+            ),
+            maxLines: 2,
+          ),
+        ),
+      ),
       body: Container(
         child: Text("Scaffold body text"),
       ),
