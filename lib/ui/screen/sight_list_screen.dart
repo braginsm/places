66ef31f202811113bc6d73716624c487e8b91bae
Serg <braginsm@gmail.com>
2021-03-22 09:43:37 +0400

    RichText и TextSpan

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -17,13 +17,36 @@ class _SightListScreenState extends State<SightListScreen> {
         toolbarHeight: 458, //высота аппбара в пикелях из фигмы
         title: Container(
           padding: EdgeInsets.fromLTRB(16, 64, 16, 322),
-          child: Text("Список интересных мест",
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              color: Color(0xff252849),
-              fontWeight: FontWeight.w700,
-              fontSize: 32,
+          child: RichText(
+            text: TextSpan(
+              text: "", 
+              children: [ //не нашел способа стилизации только первого символа TextSpan по этому разделил на подстроки, для их стилизации
+                TextSpan(
+                  text: "С",
+                  style: TextStyle(
+                    color: Color(0xff4CAF50)
+                  )
+                ),
+                TextSpan(
+                  text: "писок "
+                ),
+                TextSpan(
+                  text: "и",
+                  style: TextStyle(
+                    color: Color(0xfffcdd3d)
+                  )
+                ),
+                TextSpan(
+                  text: "нтересных мест"
+                ),
+              ],
+              style: TextStyle(
+                color: Color(0xff252849),
+                fontWeight: FontWeight.w700,
+                fontSize: 32,
+              ),
             ),
+            textAlign: TextAlign.left,
             maxLines: 2,
           ),
         ),
