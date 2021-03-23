8cc5557ddd10f8893bd22db51bb68d7bd13d82eb
Serg <braginsm@gmail.com>
2021-03-23 09:02:03 +0400

    1. Моковые данные

---
+++ lib/domain/sight.dart
@@ -0,0 +1,9 @@
+class Sight {
+  String name = ""; //-  название достопримечательности
+  double lat = 0, lon = 0; // - координаты места
+  String url = ""; // - путь до фотографии в интернете
+  String details = ""; // - описание достопримечательности
+  String type = ""; // - тип достопримечательности.
+
+  Sight({name, double lat, double lon, String url, String details, String type});
+}
