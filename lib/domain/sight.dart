1fdf51f421beca97ff01f5e008732bf45443cbf9
Сергей Брагин <braginsm@gmail.com>
2021-03-31 00:04:05 +0400

    список мест

--- lib/domain/sight.dart
+++ lib/domain/sight.dart
@@ -1,14 +1,24 @@
 class Sight {
   /// название достопримечательности
   String name = "";
+
   /// координаты места
   double lat = 0, lon = 0;
+
   ///путь до фотографии в интернете
   String url = "";
+
   ///описание достопримечательности
   String details = "";
+
   ///тип достопримечательности
   String type = "";
 
-  Sight({this.name, this.lat, this.lon, this.url, this.details, this.type});
+  ///Хочу посетить
+  bool wontVisit = false;
+
+  ///Посещал
+  bool visit = false;
+
+  Sight({this.name, this.lat, this.lon, this.url, this.details, this.type, this.wontVisit, this.visit});
 }
--- lib/ui/screen/visiting.dart
+++ lib/ui/screen/visiting.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:places/ui/res/text_styles.dart';
+import 'package:places/mocks.dart';
+import 'package:places/domain/sight.dart';
 
 class VisitingScreen extends StatefulWidget {
   VisitingScreen({Key key}) : super(key: key);
@@ -9,6 +11,10 @@ class VisitingScreen extends StatefulWidget {
 }
 
 class _VisitingScreenState extends State<VisitingScreen> {
+
+  final wontList = mocks.where((f) => f.wontVisit);
+  final visitList = mocks.where((f) => f.visit);
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -35,10 +41,7 @@ class _VisitingScreenState extends State<VisitingScreen> {
                 borderRadius: BorderRadius.circular(40),
               ),
               child: TabBar(
-                tabs: [
-                  Tab(text: "Хочу посетить"),
-                  Tab(text: "Посетил")
-                ],
+                tabs: [Tab(text: "Хочу посетить"), Tab(text: "Посетил")],
                 unselectedLabelColor: Color(0xff7C7E92).withOpacity(0.56),
                 labelColor: Colors.white,
                 unselectedLabelStyle: textBold,
@@ -55,11 +58,194 @@ class _VisitingScreenState extends State<VisitingScreen> {
         ),
         body: TabBarView(
           children: [
-            Text("0"),
-            Text("1"),
+            wontList.length > 0 ? Container(
+              margin: EdgeInsets.all(16),
+              width: double.infinity,
+              child: Column(children: [
+                for (var item in wontList)
+                  VisitItem(item)
+              ])
+            ) : Center(
+                child: Column(
+                  children: [
+                    Container(
+                      width: 64,
+                      height: 64,
+                      color: Color(0xff7C7E92
+).withOpacity(0.56),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 24),
+                      child: Text(
+                        "Пусто",
+                        style: textMedium18.copyWith(color: Color(0xff7C7E92
+).withOpacity(0.56))
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 53, vertical: 8),
+                      child: Text(
+                        "Отмечайте понравившиеся места и они появиятся здесь.",
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                        style: textRegular.copyWith(color: Color(0xff7C7E92
+).withOpacity(0.56)),
+                      ),
+                    ),
+                  ]
+                ),
+              ),
+            Container(
+              margin: EdgeInsets.all(16),
+              width: double.infinity,
+              child: Column(children: [
+                for (var item in visitList)
+                  VisitItem(item)
+              ]),
+            ),
           ],
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+          selectedItemColor: Color(0xff252849),
+        ),
       ),
+    );
+  }
+}
+
+class VisitItem extends StatelessWidget {
+  final Sight sight;
+  const VisitItem(this.sight, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(bottom: 16),
+      child: Column(children: [
+        Container(
+          child: Stack(
+            children: [
+              Container(
+                  width: double.infinity,
+                  height: 96,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16),
+                    ),
+                    child: Image.network(
+                      sight.url,
+                      fit: BoxFit.fitWidth,
+                      loadingBuilder: (BuildContext context, Widget child,
+                          ImageChunkEvent loadingProgress) {
+                        if (loadingProgress == null) return child;
+                        return Center(
+                          child: CircularProgressIndicator(
+                            value: loadingProgress.expectedTotalBytes != null
+                                ? loadingProgress.cumulativeBytesLoaded /
+                                    loadingProgress.expectedTotalBytes
+                                : null,
+                          ),
+                        );
+                      },
+                    ),
+                  )),
+              Container(
+                margin: EdgeInsets.only(top: 16, left: 16),
+                child: Text(
+                  sight.type,
+                  style: textRegular.copyWith(color: Colors.white),
+                ),
+              ),
+              Positioned(
+                top: 18,
+                right: 18,
+                child: Container(
+                  // Контейнер кнопки сердечка
+                  color: Colors.white,
+                  width: 20,
+                  height: 20,
+                ),
+              ),
+              Positioned(
+                top: 18,
+                right: 56,
+                child: Container(
+                  // Контейнер кнопки сердечка
+                  color: Colors.white,
+                  width: 20,
+                  height: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          height: 102,
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              color: Color(0xfff5f5f5),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+              )),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                child: Text(
+                  sight.name,
+                  style: textMedium16.copyWith(color: Color(0xff3B3E5B)),
+                  textAlign: TextAlign.left,
+                  maxLines: 2,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 2),
+                width: double.infinity,
+                height: 28,
+                child: Text(
+                  sight.wontVisit
+                      ? "Запланировано на 12 окт. 2020"
+                      : "Цель достигнута 12 окт. 2020",
+                  style: textRegular.copyWith(
+                      color: Color(sight.wontVisit ? 0xff4CAF50 : 0xff7C7E92)),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 2),
+                width: double.infinity,
+                child: Text(
+                  "закрыто до 09:00",
+                  style: textRegular.copyWith(color: Color(0xff7C7E92)),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
