52710533704dd7edde341e03f2491d3b62fbdc35
Сергей Брагин <braginsm@gmail.com>
2021-04-11 21:53:25 +0400

    КОнтроллеры и методы очистки полей

--- lib/ui/screen/add_sight.dart
+++ lib/ui/screen/add_sight.dart
@@ -10,20 +10,29 @@ class AddSightScreen extends StatefulWidget {
 }
 
 class _AddSightScreenState extends State<AddSightScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController latController = TextEditingController();
+  TextEditingController lonController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: TextButton(
           onPressed: () {
-            print("Отмена");
+            nameController.clear();
+            latController.clear();
+            lonController.clear();
+            descriptionController.clear();
           },
           child: Container(
-            child: Text(
-              "Отмена",
-              style: TextStyleSet().textMedium16.copyWith(color: Theme.of(context).hintColor),
-            )
-          ),
+              child: Text(
+            "Отмена",
+            style: TextStyleSet()
+                .textMedium16
+                .copyWith(color: Theme.of(context).hintColor),
+          )),
         ),
         leadingWidth: 90,
         title: Padding(
@@ -41,7 +50,9 @@ class _AddSightScreenState extends State<AddSightScreen> {
               children: [
                 Text(
                   "КАТЕГОРИЯ",
-                  style: TextStyleSet().textRegular.copyWith(color: Theme.of(context).unselectedWidgetColor),
+                  style: TextStyleSet()
+                      .textRegular
+                      .copyWith(color: Theme.of(context).unselectedWidgetColor),
                 ),
                 Padding(
                   padding: EdgeInsets.symmetric(vertical: 14),
@@ -50,9 +61,14 @@ class _AddSightScreenState extends State<AddSightScreen> {
                     children: [
                       Text(
                         "Не выбрано",
-                        style: TextStyleSet().textRegular16.copyWith(color: Theme.of(context).hintColor),
+                        style: TextStyleSet()
+                            .textRegular16
+                            .copyWith(color: Theme.of(context).hintColor),
                       ),
-                      Icon(Icons.arrow_forward_ios, size: 10,)
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 10,
+                      )
                     ],
                   ),
                 ),
@@ -62,7 +78,9 @@ class _AddSightScreenState extends State<AddSightScreen> {
                 ),
                 Text(
                   "НАЗВАНИЕ",
-                  style: TextStyleSet().textRegular.copyWith(color: Theme.of(context).unselectedWidgetColor),
+                  style: TextStyleSet()
+                      .textRegular
+                      .copyWith(color: Theme.of(context).unselectedWidgetColor),
                 ),
                 Padding(
                   padding: EdgeInsets.symmetric(vertical: 12),
@@ -70,6 +88,7 @@ class _AddSightScreenState extends State<AddSightScreen> {
                     decoration: InputDecoration(
                       hintText: 'название',
                     ),
+                    controller: nameController,
                   ),
                 ),
                 Padding(
@@ -82,16 +101,19 @@ class _AddSightScreenState extends State<AddSightScreen> {
                           children: [
                             Text(
                               "ШИРОТА",
-                              style: TextStyleSet().textRegular.copyWith(color: Theme.of(context).unselectedWidgetColor),
+                              style: TextStyleSet().textRegular.copyWith(
+                                  color:
+                                      Theme.of(context).unselectedWidgetColor),
                             ),
                             Padding(
                               padding: EdgeInsets.fromLTRB(0, 12, 8, 0),
                               child: TextField(
+                                controller: latController,
                                 decoration: InputDecoration(
                                   hintText: 'широта',
                                   suffixIcon: IconButton(
-                                    icon: Icon(Icons.cancel_rounded), 
-                                    onPressed: () => print("delete"),
+                                    icon: Icon(Icons.cancel_rounded),
+                                    onPressed: () => latController.clear(),
                                   ),
                                 ),
                                 keyboardType: TextInputType.number,
@@ -108,16 +130,19 @@ class _AddSightScreenState extends State<AddSightScreen> {
                             children: [
                               Text(
                                 "ДОЛГОТА",
-                                style: TextStyleSet().textRegular.copyWith(color: Theme.of(context).unselectedWidgetColor),
+                                style: TextStyleSet().textRegular.copyWith(
+                                    color: Theme.of(context)
+                                        .unselectedWidgetColor),
                               ),
                               Padding(
                                 padding: EdgeInsets.only(top: 12),
                                 child: TextField(
+                                  controller: lonController,
                                   decoration: InputDecoration(
                                     hintText: 'долгота',
                                     suffixIcon: IconButton(
-                                      icon: Icon(Icons.cancel_rounded), 
-                                      onPressed: () => print("delete"),
+                                      icon: Icon(Icons.cancel_rounded),
+                                      onPressed: () => lonController.clear(),
                                     ),
                                   ),
                                   keyboardType: TextInputType.number,
@@ -134,17 +159,21 @@ class _AddSightScreenState extends State<AddSightScreen> {
                   padding: EdgeInsets.fromLTRB(0, 3, 0, 25),
                   child: Text(
                     "Указать на карте",
-                    style: TextStyleSet().textMedium16.copyWith(color: Theme.of(context).accentColor),
+                    style: TextStyleSet()
+                        .textMedium16
+                        .copyWith(color: Theme.of(context).accentColor),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.symmetric(vertical: 12),
                   child: Text(
                     "ОПИСАНИЕ",
-                    style: TextStyleSet().textRegular.copyWith(color: Theme.of(context).unselectedWidgetColor),
+                    style: TextStyleSet().textRegular.copyWith(
+                        color: Theme.of(context).unselectedWidgetColor),
                   ),
                 ),
                 TextField(
+                  controller: descriptionController,
                   maxLines: 4,
                   decoration: InputDecoration(
                     hintText: "введите текст",
@@ -161,8 +190,9 @@ class _AddSightScreenState extends State<AddSightScreen> {
                   padding: EdgeInsets.symmetric(vertical: 12),
                   child: Text(
                     "СОЗДАТЬ",
-                    style: TextStyleSet().textBold
-                      .copyWith(color: Theme.of(context).canvasColor),
+                    style: TextStyleSet()
+                        .textBold
+                        .copyWith(color: Theme.of(context).canvasColor),
                   ),
                 ),
               ),
