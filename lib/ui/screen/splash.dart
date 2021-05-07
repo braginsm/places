f1fcb0871d8e7d44a3a8e7880947b7e7d4fe9d97
Serg <braginsm@gmail.com>
2021-05-07 11:53:28 +0400

    refactor

--- lib/ui/screen/splash.dart
+++ lib/ui/screen/splash.dart
@@ -41,9 +41,7 @@ class _SplashScreenState extends State<SplashScreen> {
           Navigator.pushReplacement(
             context,
             MaterialPageRoute(
-              builder: (context) {
-                return OnboardingScreen();
-              },
+              builder: (_) => OnboardingScreen(),
             ),
           );
         } else
