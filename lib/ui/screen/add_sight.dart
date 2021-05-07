fcc013e624e69c0677189d1780a8bda15f90ee31
Serg <braginsm@gmail.com>
2021-05-07 09:33:15 +0400

    Модалка добавления фотографии

--- lib/ui/screen/add_sight.dart
+++ lib/ui/screen/add_sight.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:places/domain/sight.dart';
 import 'package:places/mocks.dart';
-import 'package:places/ui/res/images.dart';
 import 'package:places/ui/res/text_styles.dart';
+import 'package:places/ui/screen/widgets/add_image_item.dart';
 import 'package:places/ui/screen/widgets/delimer.dart';
 import 'package:provider/provider.dart';
 
@@ -51,191 +50,193 @@ class _AddSightScreenState extends State<AddSightScreen> {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 100,
-                  padding: EdgeInsets.symmetric(vertical: 8),
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: context.watch<AddSightState>().images.length,
-                    itemBuilder: (context, index) {
-                      final img = context.watch<AddSightState>().images[index];
-                      return AddImageItem(img: img);
-                    },
-                  ),
-                ),
-                Text(
-                  "КАТЕГОРИЯ",
-                  style: TextStyleSet()
-                      .textRegular
-                      .copyWith(color: Theme.of(context).unselectedWidgetColor),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 14),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Не выбрано",
-                        style: TextStyleSet()
-                            .textRegular16
-                            .copyWith(color: Theme.of(context).hintColor),
-                      ),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 10,
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 24),
-                  child: Delimer(),
-                ),
-                Text(
-                  "НАЗВАНИЕ",
-                  style: TextStyleSet()
-                      .textRegular
-                      .copyWith(color: Theme.of(context).unselectedWidgetColor),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  child: TextField(
-                    //autofocus: true,
-                    focusNode: nameNode,
-                    decoration: InputDecoration(
-                      hintText: 'название',
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 100,
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: context.watch<AddSightState>().images.length,
+                      itemBuilder: (context, index) {
+                        final img = context.watch<AddSightState>().images[index];
+                        return AddImageItem(img: img);
+                      },
                     ),
-                    controller: nameController,
-                    onSubmitted: (value) => latNode.requestFocus(),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "ШИРОТА",
-                              style: TextStyleSet().textRegular.copyWith(
-                                  color:
-                                      Theme.of(context).unselectedWidgetColor),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(0, 12, 8, 0),
-                              child: TextField(
-                                controller: latController,
-                                decoration: InputDecoration(
-                                  hintText: 'широта',
-                                  suffixIcon: IconButton(
-                                    icon: Icon(Icons.cancel_rounded),
-                                    onPressed: () => latController.clear(),
-                                  ),
-                                ),
-                                keyboardType: TextInputType.number,
-                                focusNode: latNode,
-                                onSubmitted: (value) => lonNode.requestFocus(),
-                              ),
-                            ),
-                          ],
+                  Text(
+                    "КАТЕГОРИЯ",
+                    style: TextStyleSet()
+                        .textRegular
+                        .copyWith(color: Theme.of(context).unselectedWidgetColor),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 14),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Не выбрано",
+                          style: TextStyleSet()
+                              .textRegular16
+                              .copyWith(color: Theme.of(context).hintColor),
                         ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 10,
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 24),
+                    child: Delimer(),
+                  ),
+                  Text(
+                    "НАЗВАНИЕ",
+                    style: TextStyleSet()
+                        .textRegular
+                        .copyWith(color: Theme.of(context).unselectedWidgetColor),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    child: TextField(
+                      //autofocus: true,
+                      focusNode: nameNode,
+                      decoration: InputDecoration(
+                        hintText: 'название',
                       ),
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 8),
+                      controller: nameController,
+                      onSubmitted: (value) => latNode.requestFocus(),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    child: Row(
+                      children: [
+                        Expanded(
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                "ДОЛГОТА",
+                                "ШИРОТА",
                                 style: TextStyleSet().textRegular.copyWith(
-                                    color: Theme.of(context)
-                                        .unselectedWidgetColor),
+                                    color:
+                                        Theme.of(context).unselectedWidgetColor),
                               ),
                               Padding(
-                                padding: EdgeInsets.only(top: 12),
+                                padding: EdgeInsets.fromLTRB(0, 12, 8, 0),
                                 child: TextField(
-                                  controller: lonController,
-                                  focusNode: lonNode,
-                                  onSubmitted: (value) =>
-                                      descriptionNode.requestFocus(),
+                                  controller: latController,
                                   decoration: InputDecoration(
-                                    hintText: 'долгота',
+                                    hintText: 'широта',
                                     suffixIcon: IconButton(
                                       icon: Icon(Icons.cancel_rounded),
-                                      onPressed: () => lonController.clear(),
+                                      onPressed: () => latController.clear(),
                                     ),
                                   ),
                                   keyboardType: TextInputType.number,
+                                  focusNode: latNode,
+                                  onSubmitted: (value) => lonNode.requestFocus(),
                                 ),
                               ),
                             ],
                           ),
                         ),
-                      ),
-                    ],
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "ДОЛГОТА",
+                                  style: TextStyleSet().textRegular.copyWith(
+                                      color: Theme.of(context)
+                                          .unselectedWidgetColor),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 12),
+                                  child: TextField(
+                                    controller: lonController,
+                                    focusNode: lonNode,
+                                    onSubmitted: (value) =>
+                                        descriptionNode.requestFocus(),
+                                    decoration: InputDecoration(
+                                      hintText: 'долгота',
+                                      suffixIcon: IconButton(
+                                        icon: Icon(Icons.cancel_rounded),
+                                        onPressed: () => lonController.clear(),
+                                      ),
+                                    ),
+                                    keyboardType: TextInputType.number,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 3, 0, 25),
-                  child: Text(
-                    "Указать на карте",
-                    style: TextStyleSet()
-                        .textMedium16
-                        .copyWith(color: Theme.of(context).accentColor),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 3, 0, 25),
+                    child: Text(
+                      "Указать на карте",
+                      style: TextStyleSet()
+                          .textMedium16
+                          .copyWith(color: Theme.of(context).accentColor),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  child: Text(
-                    "ОПИСАНИЕ",
-                    style: TextStyleSet().textRegular.copyWith(
-                        color: Theme.of(context).unselectedWidgetColor),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    child: Text(
+                      "ОПИСАНИЕ",
+                      style: TextStyleSet().textRegular.copyWith(
+                          color: Theme.of(context).unselectedWidgetColor),
+                    ),
                   ),
-                ),
-                TextField(
-                  controller: descriptionController,
-                  focusNode: descriptionNode,
-                  onSubmitted: (value) => descriptionNode.unfocus(),
-                  maxLines: 4,
-                  decoration: InputDecoration(
-                    hintText: "введите текст",
+                  TextField(
+                    controller: descriptionController,
+                    focusNode: descriptionNode,
+                    onSubmitted: (value) => descriptionNode.unfocus(),
+                    maxLines: 4,
+                    decoration: InputDecoration(
+                      hintText: "введите текст",
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(vertical: 8),
-              child: ElevatedButton(
-                onPressed: () {
-                  /// Сохранение нового места
-                  newSight.name = nameController.text;
-                  newSight.lat = double.parse(latController.text);
-                  newSight.lon = double.parse(lonController.text);
-                  newSight.details = descriptionController.text;
-                  mocks.add(newSight);
-                },
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  child: Text(
-                    "СОЗДАТЬ",
-                    style: TextStyleSet()
-                        .textBold
-                        .copyWith(color: Theme.of(context).canvasColor),
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 8),
+                child: ElevatedButton(
+                  onPressed: () {
+                    /// Сохранение нового места
+                    newSight.name = nameController.text;
+                    newSight.lat = double.parse(latController.text);
+                    newSight.lon = double.parse(lonController.text);
+                    newSight.details = descriptionController.text;
+                    mocks.add(newSight);
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    child: Text(
+                      "СОЗДАТЬ",
+                      style: TextStyleSet()
+                          .textBold
+                          .copyWith(color: Theme.of(context).canvasColor),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -256,66 +257,5 @@ class AddSightState with ChangeNotifier {
   void removeImage(img) {
     images.remove(img);
     notifyListeners();
-  }
-}
-
-class AddImageItem extends StatefulWidget {
-  final String img;
-  AddImageItem({Key key, this.img}) : super(key: key);
-
-  @override
-  _AddImageItemState createState() => _AddImageItemState();
-}
-
-class _AddImageItemState extends State<AddImageItem> {
-  @override
-  Widget build(BuildContext context) {
-    return (widget.img == null || widget.img.isEmpty)
-        ? IconButton(
-            icon: SvgPicture.asset(ImagesPaths.plus),
-            iconSize: 72,
-            onPressed: () => print('add'),
-          )
-        : Dismissible(
-            key: ValueKey(widget.img),
-            direction: DismissDirection.up,
-            onDismissed: (direction) {
-              context.read<AddSightState>().removeImage(widget.img);
-            },
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Stack(
-                children: [
-                  GestureDetector(
-                    onTap: () => print(widget.img),
-                    child: Container(
-                      width: 72,
-                      height: 72,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        image: DecorationImage(
-                            image: Image.network(widget.img).image,
-                            fit: BoxFit.fill),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.cancel,
-                        color: Theme.of(context).backgroundColor,
-                      ),
-                      onPressed: () {
-                        context.read<AddSightState>().removeImage(widget.img);
-                      },
-                      padding: EdgeInsets.all(0),
-                    ),
-                    right: -6,
-                    top: -6,
-                  ),
-                ],
-              ),
-            ),
-          );
   }
 }
---
+++ lib/ui/screen/widgets/add_image_item.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:places/ui/res/images.dart';
+import 'package:places/ui/res/text_styles.dart';
+import 'package:provider/provider.dart';
+
+import '../add_sight.dart';
+
+class AddImageItem extends StatefulWidget {
+  final String img;
+  AddImageItem({Key key, this.img}) : super(key: key);
+
+  @override
+  _AddImageItemState createState() => _AddImageItemState();
+}
+
+class _AddImageItemState extends State<AddImageItem> {
+  void _showAddPhoto() {
+    showDialog(
+        context: context,
+        builder: (_) {
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16,),
+            //color: Colors.transparent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).backgroundColor,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(right: 16),
+                              child: SvgPicture.asset(ImagesPaths.camera),
+                            ),
+                            Text("Камера"),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(right: 16),
+                              child: SvgPicture.asset(ImagesPaths.photo),
+                            ),
+                            Text("Фотография"),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(right: 16),
+                              child: SvgPicture.asset(ImagesPaths.file),
+                            ),
+                            Text("Файл"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 8),
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Text(
+                        "ОТМЕНА",
+                        style: TextStyleSet().textBold.copyWith(color: Theme.of(context).accentColor,),
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Theme.of(context).backgroundColor),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return (widget.img == null || widget.img.isEmpty)
+        ? IconButton(
+            icon: SvgPicture.asset(ImagesPaths.plus),
+            iconSize: 72,
+            onPressed: _showAddPhoto,
+          )
+        : Dismissible(
+            key: ValueKey(widget.img),
+            direction: DismissDirection.up,
+            onDismissed: (direction) {
+              context.read<AddSightState>().removeImage(widget.img);
+            },
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Stack(
+                children: [
+                  GestureDetector(
+                    onTap: () => print(widget.img),
+                    child: Container(
+                      width: 72,
+                      height: 72,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        image: DecorationImage(
+                            image: Image.network(widget.img).image,
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.cancel,
+                        color: Theme.of(context).backgroundColor,
+                      ),
+                      onPressed: () {
+                        context.read<AddSightState>().removeImage(widget.img);
+                      },
+                      padding: EdgeInsets.all(0),
+                    ),
+                    right: -6,
+                    top: -6,
+                  ),
+                ],
+              ),
+            ),
+          );
+  }
+}
