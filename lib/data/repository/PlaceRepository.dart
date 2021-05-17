ff94085566ea2bbc495e4e58d43d16f79e455d73
Сергей Брагин <braginsm@gmail.com>
2021-05-17 18:35:05 +0400

    PLace deleteById

--- lib/data/repository/PlaceRepository.dart
+++ lib/data/repository/PlaceRepository.dart
@@ -1,11 +1,23 @@
+import 'package:dio/dio.dart';
 import 'package:places/data/model/Place.dart';
 import 'package:places/data/repository/repository.dart';
 
 class PlaceRepository extends Repository {
   Future<Place> getById(String id) async {
     try {
-      var res = await dio.get("/place/$id");
-      return Place.fromJson(res.data);
+      Response res = await dio.get("/place/$id");
+      if (res.statusCode == 200) return Place.fromJson(res.data);
+      throw Exception("Место с id = $id не найдено");
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<bool> deleteById(String id) async {
+    try {
+      Response res = await dio.delete("/place/$id");
+      if (res.statusCode == 200) return true;
+      throw Exception("Место с id = $id не найдено");
     } catch (e) {
       throw e;
     }
