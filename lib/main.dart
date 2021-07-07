7dd6ab9dff5f607ffa9783ffcf7cc324bab36ee5
Сергей Брагин <braginsm@gmail.com>
2021-07-07 15:18:53 +0400

    redebug

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,14 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:mwwm/mwwm.dart';
 import 'package:places/data/interactor/PlaceInteractor.dart';
 import 'package:places/data/interactor/SearchInteractor.dart';
-import 'package:places/data/model/Place.dart';
 import 'package:places/data/redux/middleware/search_middleware.dart';
 import 'package:places/data/redux/reducer/reducer.dart';
 import 'package:places/data/redux/state/app_state.dart';
 import 'package:places/ui/res/themes.dart';
-import 'package:places/ui/screen/add_place/add_place_screen.dart';
 import 'package:places/ui/screen/add_place/add_place_wm.dart';
 import 'package:places/ui/screen/add_place/default_error_handler.dart';
 import 'package:places/ui/screen/add_sight.dart';
+import 'package:places/ui/screen/splash.dart';
 import 'package:provider/provider.dart';
 import 'package:redux/redux.dart';
 import 'data/interactor/SettingsInteractor.dart';
@@ -82,8 +81,7 @@ class App extends StatelessWidget {
       store: store,
       child: MaterialApp(
         theme: context.watch<MainState>().theme,
-        //home: SplashScreen(),
-        home: AddPlaceScreen(widgetModelBuilder: (BuildContext context) { return context.read<AddPlaceScreenWidgetModel>(); },),
+        home: SplashScreen(),
         title: "Places",
       ),
     );
