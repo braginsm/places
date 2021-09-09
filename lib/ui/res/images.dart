f77ca266e76764bb661b1429f93331e66688cc28
Serg <braginsm@gmail.com>
2021-09-09 14:14:41 +0400

    rename svg

--- lib/ui/res/images.dart
+++ lib/ui/res/images.dart
@@ -1,14 +1,14 @@
 class ImagesPaths {
-  static String tickChoice = "res/images/tick_choice.svg";
+  static String tickChoice = "res/images/tickChoice.svg";
   static List<String> ticks = [
-    for (var i = 0; i < 6; i++) "res/images/tick_$i.svg"
+    for (var i = 0; i < 6; i++) "res/images/tick$i.svg"
   ];
   static List<String> tutorial = [
-    for (var i = 0; i < 3; i++) "res/images/tutorial_$i.svg"
+    for (var i = 0; i < 3; i++) "res/images/tutorial$i.svg"
   ];
   static String info = "res/images/info.svg";
-  static String buildRoute = "res/images/build_route.svg";
-  static String toPlan = "res/images/to_plan.svg";
+  static String buildRoute = "res/images/buildRoute.svg";
+  static String toPlan = "res/images/toPlan.svg";
   static String calendar = "res/images/calendar.svg";
   static String share = "res/images/share.svg";
   static String go = "res/images/go.svg";
@@ -24,17 +24,17 @@ class ImagesPaths {
   static String splash = "res/images/splash.svg";
   static String imgPlaceholder = "res/images/placeholder.png";
   static String heart = "res/images/heart.svg";
-  static String heartFull = "res/images/heart_full.svg";
+  static String heartFull = "res/images/heartFull.svg";
   static String ellipse = "res/images/ellipse.png";
   static String list = "res/images/list.svg";
   static String listFull = "res/images/list.svg";
-  static String map = "res/images/map_full.svg";
-  static String mapFull = "res/images/map_full.svg";
+  static String map = "res/images/mapFull.svg";
+  static String mapFull = "res/images/mapFull.svg";
   static String settings = "res/images/settings.svg";
   static String settingsFull = "res/images/settings.svg";
   static String refresh = "res/images/refresh.svg";
   static String geolocation = "res/images/geolocation.svg";
   static String close = "res/images/close.svg";
-  static String iAmHere = "res/images/i_am_here.png";
+  static String iAmHere = "res/images/iAmHere.png";
   static String mapPoint = "res/images/point.png";
 }
